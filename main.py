c30b081af806c161fcd9d142495a7277f9a980a0
Will Harris-Braun <willharrisbraun@gmail.com>
2019-02-15 18:41:54 -0500

    Update to color calculation
    Colors now cycle blue -> green -> orange -> red -> purple. Also with the 
    correct ratios according to plans

--- main.py
+++ main.py
@@ -47,9 +47,28 @@ sgp30.set_iaq_baseline(0x8973, 0x8aae)
 def avg(lst):
     return sum(lst)/BLUR_AMOUNT
 
+# returns a value on the color wheel described in the wheel() function
+# based a given CO2 level
+def getWheelValueFromCO2(CO2):
+    if inRange(CO2, 400,450):
+        return mapFromTo(CO2, 400,450, 210,255)
+    elif inRange(CO2, 450,600):
+        return mapFromTo(CO2, 450,600, 0,55)
+    elif inRange(CO2, 600,1300):
+        return mapFromTo(CO2, 600,1300, 55,85)
+    elif inRange(CO2, 1300,2500):
+        return mapFromTo(CO2, 1300,2500, 85,95)
+    elif inRange(CO2, 2500,5000):
+        return mapFromTo(CO2, 2500,5000, 95,120)
+    else:
+        return 120
+
+# returns a color
+def getColorFromCO2(CO2):
+    return wheel(getWheelValueFromCO2(CO2))
+
 # set up list of last values to update and average
 vals = [400 for x in range(BLUR_AMOUNT)]
-
 batLow = False
 tick = int(time.monotonic())
 
@@ -84,11 +103,11 @@ while True:
     print(blurredVal)
     # print(voltage)
 
-    displayWheelPos = mapFromTo(blurredVal, 400,1900, 0,255)
-    displayColor = wheel(displayWheelPos)
+    displayColor = getColorFromCO2(blurredVal)
+
+    # pulse the display color if in low battery mode
     if batLow == True:
         pixels.fill(pulse(displayColor, LOWBAT_PULSE_SPEED, LOWBAT_PULSE_DEPTH))
+    # otherwise just display the color
     else:
         pixels.fill(displayColor)
-
-#    pixels.show()
